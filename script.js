a1a061c51a8f051e96554aaacfa40ec556aa0745
Md Sayan Akram <mdsayanakram@gmail.com>
2025-07-02 13:53:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+const p1 = localStorage.getItem("player1");
+const p2 = localStorage.getItem("player2");
+const tieVal = localStorage.getItem("tieBreaker");
+
+document.querySelector("h3").innerText = `✖ : ${p1}  |  ◯: ${p2};`
+
 const clr1 = `#B75E77`; //player 1, shade of red
 const clr2 = `#B0D7FF`; //player 2, shade of blue
 let clr = clr2;
-let cross = `<i class="fa-solid fa-xmark logo"></i>`; //use with clr 2
-let circle = `<i class="fa-regular fa-circle logo"></i>`; //use with clr 1
+let cross = `<i class="fa-solid fa-xmark logo" style="font-size: 7vw;"></i>`; //use with clr 2
+let circle = `<i class="fa-regular fa-circle logo" style="font-size: 6vw;"></i>`; //use with clr 1
 let win = false;
 const queue = [];
 const player1 = [];
 const player2 = [];
+
+//currect player and symbol
+const toss = localStorage.getItem("toss") === "1";
+let symbol = toss ? cross : circle;
+
+let currPlayer;
+if(toss) currPlayer = player1; // toss true = player1 starts
+else currPlayer = player2;
 
 const winArray = [
     [0,1,2],
@@ -22,6 +36,16 @@ const winArray = [
 function toggleClr(clr){
     if(clr === clr2) return clr1;
     else return clr2;
+}
+
+function togglePlayer(plr){
+    if(plr === player1) return player2;
+    else return player1;
+}
+
+function toggleSymbol(sym){
+    if(sym === cross) return circle;
+    else return cross;
 }
 
 //this function return an array
@@ -52,6 +76,7 @@ function glowBox(array){
     });
 }
 
+
 //create a fnction that retuens the box/ box index that is clikcked
 //boxes is an array of all the 9 boxes
 const boxes = document.querySelectorAll(".nineBox"); 
@@ -63,30 +88,19 @@ boxes.forEach((box, index) => {
         box.style.backgroundColor = clr;
         queue.push(index);
 
+        box.innerHTML = symbol;
+        symbol = toggleSymbol(symbol);
+
         //push the element and check for win
-        if(clr === clr2){
-            box.innerHTML = cross;
-            player1.push(index);
+        currPlayer.push(index);
 
-            let tempRow = checkWin(player1);
-            if(tempRow.length == 3){
-                document.querySelector("#winner").innerText = "Player1 Won";
-                glowBox(tempRow);
-                win = true;
-                return;
-            }
-        }
-        else{
-            box.innerHTML = circle;
-            player2.push(index);
-
-            let tempRow = checkWin(player2);
-            if (tempRow.length == 3){
-                document.querySelector("#winner").innerText = "Player2 Won";
-                glowBox(tempRow);
-                win = true;
-                return;
-            }
+        let tempRow = checkWin(currPlayer);
+        if(tempRow.length == 3){
+            if(clr == clr2) document.querySelector("#winner").innerText = `${p2} Won`;
+            else document.querySelector("#winner").innerText = `${p1} Won`;
+            glowBox(tempRow);
+            win = true;
+            return;
         }
 
         clr = toggleClr(clr);
@@ -96,17 +110,21 @@ boxes.forEach((box, index) => {
         // console.log("player2 ",player2);
 
         //check for tie
+        //let tie = 2;
         if(queue.length === 9){
-            let idx = queue.shift();
-            boxes[idx].style.backgroundColor = "#023e8a4D";
-            boxes[idx].innerHTML = "";
-            idx = queue.shift();
-            boxes[idx].style.backgroundColor = "#023e8a4D";
-            boxes[idx].innerHTML = "";
-
-            player1.shift();
-            player2.shift();
+            let tempPlayer = currPlayer;
+            for(let i=0;i<tieVal;i++){
+                let idx = queue.shift();
+                boxes[idx].style.backgroundColor = "#023e8a4D";
+                boxes[idx].innerHTML = "";
+    
+                tempPlayer.shift();
+                tempPlayer = togglePlayer(tempPlayer);
+            }
         }
+
+        //toggle the player
+        currPlayer = togglePlayer(currPlayer);
     });
 });
 
